71d2d1460b1fa5024e98267e7c4e3521f158d02e
soroush <soroush.najafi89@gmail.com>
2022-10-24 12:05:33 +0330

    writing tests for pages app completed

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url_reverse(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'pages/home.html')
+
+
+class AboutUsPageTest(TestCase):
+
+    def test_about_us_page_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_url_reverse(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_content(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, 'About Us Page')
+
+    def test_about_us_page_template_used(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertTemplateUsed(response, 'pages/aboutus.html')
