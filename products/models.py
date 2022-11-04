ff1931845ee6480386277ff5749008c9f072c3f5
soroush <soroush.najafi89@gmail.com>
2022-11-04 20:02:45 +0330

    image field for product created and media implemented

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=5)
     active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='product/product_cover/', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
