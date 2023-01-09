c18cc29a6cc2b20f8884dc42591c8f0d449292ed
soroush <soroush.najafi89@gmail.com>
2023-01-09 16:32:24 +0330

    app order created and model order and orderitem created

---
+++ orders/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    is_paid = models.BooleanField(default=False)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=500)
+    phone_number = models.CharField(max_length=50)
+    order_notes = models.CharField(max_length=500, blank=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order number: {self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+
+    def __str__(self):
+        return f'OrderItem: product: {self.product}, quantity: {self.quantity}, price: {self.price}'
