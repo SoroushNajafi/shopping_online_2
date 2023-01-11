a07d47f808ccfd452461f82edb6d7ea88b22439f
soroush <soroush.najafi89@gmail.com>
2023-01-11 12:03:25 +0330

    order create view added

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
+from .forms import OrderForm
 from cart.cart import Cart
-from cart.forms import AddToCartForm
+from .models import OrderItem
 
 
+@login_required
 def order_create_view(request):
+    cart = Cart(request)
 
-    return render(request, 'orders/order_create.html',)
+    order_form = OrderForm()
+
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+
+        if len(cart) == 0:
+            messages.warning(request, _('You can not proceed to checkout since your cart is empty'))
+            return redirect('home')
+
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=item['product_obj'],
+                    quantity=item['quantity'],
+                    price=item['product_obj'].price,
+                )
+            cart.clear()
+            messages.success(request, _('Your Order has been submitted'))
+
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
+
+    return render(request, 'orders/order_create.html', {'order_form': order_form})
